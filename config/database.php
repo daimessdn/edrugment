e76346703253d7e7875ffd17d16fa5e0acc75fe7
daimessdn <daimessdn@gmail.com>
2019-12-11 18:17:46 +0700

    re-configured database url...

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'https://db4free.net'),
+            'host' => env('DB_HOST', 'http://db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'edrugment'),
             'username' => env('DB_USERNAME', 'edrugment'),
